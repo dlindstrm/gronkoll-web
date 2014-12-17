d5a1457f1f24d116245c3f4d5fe5a5a71d911c71
Johan Kasperi <johan@kasperi.se>
2014-12-17 18:36:56 +0100

    added routing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
+var routes = require('./routes/index');
 
 var app = express();
 module.exports = app;
@@ -12,11 +13,19 @@ app.use(express.static(path.join(__dirname, 'bower_components/normalize.css')));
 /**
  * Routes
  */
-app.use('/', function(req, res) {
-  res.sendFile(path.join(__dirname, "public/templates/index.html"));
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.set('port', 1234);
+app.use(function(err, req, res, next){
+  res.status(err.status || 500).redirect('/');
+});
+
+app.set('port', 3002);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
